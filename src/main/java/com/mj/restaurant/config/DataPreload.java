2dfc42d6ae30f35e3a879f253f43708d5dd5fcdf
marek <marek.jastrzebski@outlook.com>
2019-09-24 19:40:29 +0200

    one more object added with other order status

--- src/main/java/com/mj/restaurant/config/DataPreload.java
+++ src/main/java/com/mj/restaurant/config/DataPreload.java
@@ -1,6 +1,7 @@
 package com.mj.restaurant.config;
 
 import com.mj.restaurant.model.Order;
+import com.mj.restaurant.model.OrderStatus;
 import com.mj.restaurant.repository.OrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,8 +14,11 @@ public class DataPreload implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Order o=new Order();
-        o.setOrderedItems("Esspresso Lungo");
-        ordersRepository.save(o);
+        Order o1=new Order("Esspresso Lungo");
+        Order o2=new Order("Cafe Latte");
+        o2.setStatus(OrderStatus.IN_PROGRESS);
+
+        ordersRepository.save(o1);
+        ordersRepository.save(o2);
     }
 }
